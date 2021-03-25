45c26a62ed7ed4c5b3e10f5a62e57d1e25e528e7
LeoFuna <leo.funa@gmail.com>
2021-03-25 13:52:04 -0300

    Exercicio 2

--- trybe-exercises/bloco_4/Parte_2/exercicio1.js
+++ trybe-exercises/bloco_4/Parte_2/exercicio1.js
@@ -3,3 +3,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for ( let conteudo of numbers ) {
   console.log(conteudo);
 }
+
+let soma = 0
+
+for ( let conteudo2 of numbers ) {
+  soma += conteudo2;
+}
+console.log(soma);
