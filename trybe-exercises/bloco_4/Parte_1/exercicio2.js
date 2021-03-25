a93a8e39b14aeac63083325d02b871f640cb4609
LeoFuna <leo.funa@gmail.com>
2021-03-25 13:41:39 -0300

    atualização da branch

---
+++ trybe-exercises/bloco_4/Parte_1/exercicio2.js
@@ -0,0 +1,9 @@
+const num1 = 16;
+const num2 = 13;
+
+if (num1 > num2) {
+  console.log(num1);
+}
+else {
+  console.log(num2);
+}
---
+++ trybe-exercises/bloco_4/Parte_1/exercicio7.js
@@ -0,0 +1,17 @@
+const nota = 70;
+
+if ( nota >= 90 && nota <= 100) {
+  console.log("nota A")
+}else if (nota >= 80 && nota < 90) {
+  console.log("nota B")
+}else if (nota >= 70 && nota <80) {
+  console.log("nota C")
+}else if (nota >= 60 && nota < 70) {
+  console.log("nota D")
+}else if ( nota >= 50 && nota < 60) {
+  console.log("nota E")
+}else if (nota < 50 && nota >= 0) {
+  console.log("nota F")
+}else {
+  console.log("Nota inválida")
+}
