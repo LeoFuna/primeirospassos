d2be2a5e5d587bef04a8ad500085cd34cad96482
LeoFuna <leo.funa@gmail.com>
2021-03-25 13:43:15 -0300

    ajustes master

--- trybe-exercises/bloco_4/exercicio1.js
+++
@@ -1,8 +0,0 @@
-const a = 5;
-const b = 10;
-
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
--- trybe-exercises/bloco_4/exercicio2.js
+++
@@ -1,9 +0,0 @@
-const num1 = 16;
-const num2 = 13;
-
-if (num1 > num2) {
-  console.log(num1);
-}
-else {
-  console.log(num2);
-}
