2ca7c63ad088fc1556c4fa6b60f7192e42c5d8ae
LeoFuna <leo.funa@gmail.com>
2021-05-04 13:38:20 -0300

    Finalização exercicio dia 2 bloco 8

--- trybe-exercises/bloco_8/forEach-Find-etc.js
+++ trybe-exercises/bloco_8/forEach-Find-etc.js
@@ -92,7 +92,7 @@ assert.strictEqual(smallerName(), 'Duna');
 
 //exercicio 3
 
-const expectedResult = {
+let expectedResult = {
   author: {
     birthYear: 1948,
     name: 'George R. R. Martin',
@@ -111,3 +111,93 @@ function getNamedBook() {
 assert.deepStrictEqual(getNamedBook(), expectedResult);
 
 //exercicio 4
+
+expectedResult = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947 },
+    releaseYear: 1986,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+];
+
+function booksOrderedByReleaseYearDesc() {
+  const orderedArray = books.sort((objeto1, objeto2) => objeto2.releaseYear - objeto1.releaseYear);
+  return orderedArray;
+}
+
+assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expectedResult);
+
+//exercicio 5
+
+expectedResult = false;
+
+function everyoneWasBornOnSecXX() {
+  return books.every((objeto) => objeto.author.birthYear < 2000 && objeto.author.birthYear > 1900);
+}
+
+assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult);
+
+//exercicio 6
+
+expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  return books.some((objeto) => objeto.releaseYear > 1980 && objeto.releaseYear < 1990);
+}
+
+assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult);
+
+//exercicio 7
+
+expectedResult = false;
+
+function authorUnique() {
+  return books.every((objeto) => {
+    let isEqual = 0;
+    for (let values of books) {
+      if (values.author.birthYear === objeto.author.birthYear) {
+        isEqual += 1;
+      }
+    }
+    return isEqual > 1;
+  })
+}
+
+assert.strictEqual(authorUnique(), expectedResult);
