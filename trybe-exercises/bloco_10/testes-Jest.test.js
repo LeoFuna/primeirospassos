b93f55e636681bc93818e857f6a94bae2f93ceb2
LeoFuna <leo.funa@gmail.com>
2021-05-18 16:54:48 -0300

    retirada do node modules por ser grande

--- trybe-exercises/bloco_10/testes-Jest.test.js
+++ trybe-exercises/bloco_10/testes-Jest.test.js
@@ -4,4 +4,10 @@ describe('Teste função sum', () => {
   it('Teste se o retorno de sum(4, 5) é 9', () => {
     expect(sum(4,5)).toEqual(9);
   });
+  it('Teste se o retorno de sum(0, 0) é 0', () => {
+    expect(sum(0,0)).toEqual(0);
+  });
+  it('Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
+    expect(() => {sum(4,'5')}).toThrow();
+  });
 });
