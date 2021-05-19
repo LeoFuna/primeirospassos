5381ef680c78bc5410157a938c3210176f66f8ed
LeoFuna <leo.funa@gmail.com>
2021-05-19 15:09:58 -0300

    Exercicio 2 e 3 bloco 10 dia 2

--- trybe-exercises/bloco_10/testes-assincronons.spec.js
+++ trybe-exercises/bloco_10/testes-assincronons.spec.js
@@ -1,4 +1,4 @@
-const { uppercase } = require('./testes-assincronos');
+const { uppercase, getUserName } = require('./testes-assincronos');
 
 describe('Testa retorno da função Uppercase', () => {
   it('Deve ser uma string totalmente maiúscula', done => { //Atenção ao done()
@@ -6,5 +6,35 @@ describe('Testa retorno da função Uppercase', () => {
       expect(value).toBe('OLÁ, COMO VAI VOCÊ?'); // o Expect vai dentro do callback da função!!
       done(); // Atenção ao done()
     })
+  });
+});
+
+//Exercicio 2
+describe('Testa retorno da função getUserName', () => {
+  it('Retorno deve ser Mark', () => {
+    return getUserName(4).then((objectName) => {
+      expect(objectName).toBe('Mark');
+    });
+  });
+  it('Retorno de um id nao existente deve ser Erro', () => {
+    return getUserName(7).catch((objectName) => {
+      expect(objectName.error).toBe('User with 7 not found.');
+    })
+  })
+});
+
+//Exercicio 3 (fazer o 2 com async e await)
+describe('Testa retorno da função getUserName com async/await', () => {
+  it('Retorno deve ser Mark', async () => {
+    const objectName = await getUserName(4);
+    expect(objectName).toBe('Mark');
+  });
+  it('Retorno de um id nao existente deve ser Erro', async () => {
+    try {
+      await getUserName(7);
+    } 
+    catch(error) {
+      expect(error.error).toBe('User with 7 not found.');
+    }
   });
 });
