8cb4e2a48f6eb1f58999ad7067e3e3281d1d1317
LeoFuna <leo.funa@gmail.com>
2021-05-20 15:01:53 -0300

    Ex 2 bloco 10 dia 3

--- trybe-exercises/bloco_10/jest-testes-assincr.spec.js
+++ trybe-exercises/bloco_10/jest-testes-assincr.spec.js
@@ -1,12 +1,21 @@
 let { createRandomNumber } = require('./jest-testes-assincr');
 const { } = require('@jest/globals');
 const { TestScheduler } = require('@jest/core');
+jest.mock('./jest-testes-assincr')
 
-it('Testes da função createRandomNumber', () => {
-  createRandomNumber = jest.fn().mockReturnValue(10);
+// it('Testes da função createRandomNumber', () => {
+//   createRandomNumber = jest.fn().mockReturnValue(10);
 
-  createRandomNumber();
+//   createRandomNumber();
+//   expect(createRandomNumber).toHaveBeenCalled();
+//   expect(createRandomNumber()).toBe(10);
+//   expect(createRandomNumber).toHaveBeenCalledTimes(2);
+// });
+
+it('testa a funçao create random com alterações', () => {
+  createRandomNumber.mockImplementationOnce((a,b) => (a / b));
+
+  expect(createRandomNumber(4,2)).toBe(2);
   expect(createRandomNumber).toHaveBeenCalled();
-  expect(createRandomNumber()).toBe(10);
-  expect(createRandomNumber).toHaveBeenCalledTimes(2);
-});
+  expect(createRandomNumber(4,2)).not.toBe(2);
+})
